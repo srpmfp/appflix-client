46684ebe665b0c32de990b58b24920f153b8d8c4
srpmfp <sean@mothnflame.com>
2025-01-19 19:47:24 -0500

    added new components login and sign up views

--- src/components/Main-View/Main-View.jsx
+++ src/components/Main-View/Main-View.jsx
@@ -1,34 +1,67 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
 
 
 export const MainView = () => {
+
+    // Persistent login information
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+
+
+    // User Information
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null);
+
+    //Movie information
+    const [selectedMovie, setSelectedMovie] = useState(null);
     const [movies, setMovies] = useState([]);
 
-    const [selectedMovie, setSelectedMovie] = useState(null);
 
+
+
+    // return movie info from the database GET REQ
     useEffect(() => {
+        if (!token) { return; }
         fetch(
-            "https://appflixcf-d4726ef19667.herokuapp.com/movies")
+            "https://appflixcf-d4726ef19667.herokuapp.com/movies", {
+            headers: { Authorization: `bearer ${token}` },
+        })
             .then((response) => response.json())
             .then((data) => {
                 const movieData = data.map((movie) => {
                     return {
-                        id: movie._id,
+                        id: movie.id,
                         title: movie.title,
                         genre: movie.genre,
-                        director: movie.director.name,
-                        directorBio: movie.director.bio,
-                        directorBirthday: movie.director.birthday,
+                        director:
+                        {
+                            name: movie.director.name,
+                            bio: movie.director.bio,
+                            birthday: movie.director.birthday
+                        },
                         image: movie.image,
-
                     };
                 });
                 setMovies(movieData);
             })
-    }, []);
+    }, [token]);
 
+    if (!user) {
+        return (<>
+            < LoginView onLoggedIn={(user, token) => {
+                setUser(user);
+                setToken(token);
+            }
+
+            } />
+            or
+            < SignupView />
+        </>)
+    }
     // If the list of movies is empty, the user will receive this message
     if (movies.length === 0) return
     <div className="main-view">
@@ -40,23 +73,34 @@ export const MainView = () => {
     if (selectedMovie) {
         return (<MovieView
             movie={selectedMovie}
+            key={movies.title}
             backButton={() => {
                 setSelectedMovie(null);
             }}
         />);
     }
 
+
+
     // Otherwise, the user will see the main view
     return (
-        <div className="main-view">
+        <div>
             {movies.map((movie) => (
                 <MovieCard
-                    key={movie.id}
+                    key={movie.title}
                     movie={movie}
                     onMovieClick={(newSelectedMovie) => {
                         setSelectedMovie(newSelectedMovie)
                     }}
                 />))}
+            <button onClick={() => {
+                setUser(null);
+                setToken(null);
+                localStorage.clear()
+            }}>Log Out</button>
         </div>
+
+
     )
+
 }
