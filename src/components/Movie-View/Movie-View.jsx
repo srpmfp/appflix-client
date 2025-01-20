c02b09fbaa08444fd1bdbf8fb9bb62422686827b
srpmfp <sean@mothnflame.com>
2025-01-19 19:46:24 -0500

    added correct proptypes to movie-view

--- src/components/Movie-View/Movie-View.jsx
+++ src/components/Movie-View/Movie-View.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, backButton }) => {
     return (
         <div className="movie-view">
@@ -12,14 +14,14 @@ export const MovieView = ({ movie, backButton }) => {
             </div>
             <div className="movie-director">
                 <span className="label">director: </span>
-                <span className="value">{movie.director}</span>
+                <span className="value">{movie.director.name}</span>
                 <div>
                     <span className="label">Bio: </span>
-                    <span className="value">{movie.directorBio}</span>
+                    <span className="value">{movie.director.bio}</span>
                 </div>
                 <div>
                     <span className="label">Birthday: </span>
-                    <span className="value">{movie.directorBirthday}</span>
+                    <span className="value">{movie.director.birthday}</span>
                 </div>
             </div>
             <div className="movie-poster">
@@ -27,5 +29,23 @@ export const MovieView = ({ movie, backButton }) => {
             </div>
             <button onClick={() => backButton()}>Back</button>
         </div>
+
     );
+
 }
+MovieView.propTypes = {
+    movie: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        genre: PropTypes.string,
+        director: PropTypes.shape({
+            name: PropTypes.string,
+            bio: PropTypes.string,
+            birthday: PropTypes.string
+        }),
+        image: PropTypes.string,
+        id: PropTypes.string
+    }).isRequired,
+    backButton: PropTypes.func
+}
+
+
