e8b025866b97ca932af488823d7c94aace661d5c
srpmfp <sean@mothnflame.com>
2025-01-20 20:32:04 -0500

    Movie Card Formatting

--- src/components/Movie-Card/Movie-Card.jsx
+++ src/components/Movie-Card/Movie-Card.jsx
@@ -1,33 +1,34 @@
-import { Image, Card } from 'react-bootstrap';
-import Stack from 'react-bootstrap/Stack';
+import { Image, Card, Container, Col } from 'react-bootstrap';
+import './movie-card.scss';
 import PropTypes from 'prop-types';
-export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-        <Stack direction="horizontal" gap={3}>
-            <Card style={{ width: "10rem" }} onClick={() => {
-                onMovieClick(movie)
-            }}>
-                <Image src={movie.image} thumbnail />
-                <Card.Body>
-                    <Card.Title className="p-2">{movie.title}</Card.Title>
-                    <Card.Text>{movie.genre}</Card.Text>
-                </Card.Body>
-            </Card>
-        </Stack>
-    );
-}
-MovieCard.PropTypes = {
-    movie: PropTypes.shape({
-        title: PropTypes.string,
-        director: PropTypes.shape({
-            bio: PropTypes.string,
-            name: PropTypes.string,
-            birthday: PropTypes.date
-        }),
-        genre: PropTypes.string,
-        image: PropTypes.string,
-        id: PropTypes.string,
-    }),
-    onMovieClick: PropTypes.func.isRequired
-}
 
+export const MovieCard = ({ movie, onMovieClick }) => {
+  return (
+    <Col className='movieCol'>
+      <Card
+        onClick={() => {
+          onMovieClick(movie);
+        }}>
+        <Card.Img src={movie.image} variant='top' />
+        <Card.Body className='p-0 movieCardBody text-center text-white text-outline'>
+          <Card.Title className='mTitleText movieCard text-light'>{movie.title}</Card.Title>
+          <Card.Text className='mGenreText movieCard'>{movie.genre}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
+MovieCard.PropTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    director: PropTypes.shape({
+      bio: PropTypes.string,
+      name: PropTypes.string,
+      birthday: PropTypes.date,
+    }),
+    genre: PropTypes.string,
+    image: PropTypes.string,
+    id: PropTypes.string,
+  }),
+  onMovieClick: PropTypes.func.isRequired,
+};
