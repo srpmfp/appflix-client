e8fd3d504c5adfc129f5e958817422cdc545c762
srpmfp <sean@mothnflame.com>
2025-01-24 22:37:43 -0500

    removing unused css

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { MainView } from './components/main-view/main-view.jsx';
+import { MainView } from './components/main-view/Main-View.jsx';
 import { Container } from 'react-bootstrap';
 import './index.scss';
 
 const MyFlixApp = () => {
   return (
-    <Container className='indexCont d-flex mx-0 mw-100 mh-100 align-items-stretch'>
+    <Container className='indexCont d-flex mx-0 mw-100 mh-100 '>
       <MainView />;
     </Container>
   );
