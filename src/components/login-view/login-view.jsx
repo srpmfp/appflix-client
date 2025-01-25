939a356c020889d436bb148a6ed0535d5894b012
srpmfp <sean@mothnflame.com>
2025-01-24 22:44:53 -0500

    centering form with button props added

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { Form, Card, Button, Col, Row, Col } from 'react-bootstrap';
+import { SignupView } from '../signup-view/signup-view';
 
 import './login-view.scss';
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
+  const [signUp, setSignup] = useState();
 
   const handleEvent = (e) => {
     e.preventDefault();
@@ -40,37 +43,60 @@ export const LoginView = ({ onLoggedIn }) => {
       });
   };
   return (
-    <Row className='align-items-center'>
-      <Col className='col-3 '></Col>
-      <Col className=' d-flex flex-column w-auto m-auto col-6'>
-        <label className='login-card align-self-center radius-10 w-100 text-center'>
-          Login Here
-        </label>
-        <Card className='login-card d-flex align-content-center bg-dark radius-10 '>
-          <Form onSubmit={handleEvent} className='login-card radius-10 text-dark'>
-            <Form.Group controlId='formBasicUsername' className='p-1'>
-              <Form.Label>Username:</Form.Label>
-              <Form.Control
-                type='text'
-                value={username}
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId='formBasicPassword' className='p-1'>
-              <Form.Label>Password:</Form.Label>
-              <Form.Control
-                type='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <Button type='submit justify-self-center'>Submit</Button>
-            </Form.Group>
-          </Form>
+    <Row md={1} className='align-items-center justify-content-center bg-dark rounded'>
+      {!signUp ? (
+        <Card className='bg-dark p-0'>
+          <Col className=' d-flex flex-column w-100 m-auto col-6 bg-dark'>
+            <label className='login-Label  radius-10 w-100 text-center'>Login Here</label>
+            <Card className='login-card d-flex  bg-dark radius-10 '>
+              <Form onSubmit={handleEvent} className='login-card radius-10 text-dark'>
+                <Form.Group controlId='formBasicUsername' className='p-1'>
+                  <Form.Label>Username:</Form.Label>
+                  <Form.Control
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                  />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className='p-1'>
+                  <Form.Label>Password:</Form.Label>
+                  <Form.Control
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <Button type='submit justify-self-center ' size='sm'>
+                    Submit
+                  </Button>
+                </Form.Group>
+              </Form>
+            </Card>
+          </Col>
+          <Col className='col-3 w-100 d-flex bg-dark signup-btn-container'>
+            <Button
+              className='signup-btn'
+              variant='dark'
+              onClick={(viewSignUp) => {
+                setSignup(viewSignUp);
+              }}>
+              Sign Up
+            </Button>
+          </Col>
         </Card>
-      </Col>
-      <Col className='col-3'></Col>
+      ) : (
+        <Col className='d-flex justify-content-center m-0 p-0'>
+          <SignupView
+            returnBtn={() => {
+              setSignup(null);
+            }}
+          />
+        </Col>
+      )}
     </Row>
   );
+};
+LoginView.propTypes = {
+  button: PropTypes.func.isRequired,
 };
