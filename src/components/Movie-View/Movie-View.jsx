05f5f2622a0dcafe28a517c1e819524d6242e7b2
srpmfp <sean@mothnflame.com>
2025-02-04 22:05:31 -0500

    added add and remove to movie view
    allowed for movie view to navigate back 1 after adding or removing a card

--- src/components/Movie-View/Movie-View.jsx
+++ src/components/Movie-View/Movie-View.jsx
@@ -1,35 +1,150 @@
 import PropTypes from 'prop-types';
+import { useParams, Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import { Card, Button, ListGroup } from 'react-bootstrap';
 import './movie-view.scss';
 
-export const MovieView = ({ movie, backButton }) => {
-  return (
-    <Card className='movieViewCard justify-content-center align-items-center p-1'>
-      <Button className='float-right w-25 btn-secondary'>Add</Button>
-      <Card.Body md={3} sm={6}>
-        <Card.Img
-          md={3}
-          sm={6}
-          xs={12}
-          src={movie.image}
-          style={{ width: '50%' }}
-          alt='Movie Title Image'
-        />
-        <Card.Title className='text-light'> Title: {movie.title}</Card.Title>
-        <ListGroup className='movieViewList'>
-          <ListGroup.Item> Genre: {movie.genre}</ListGroup.Item>
-          <ListGroup.Item> Director: {movie.director.name}</ListGroup.Item>
-          <ListGroup.Item>Bio: {movie.director.bio}</ListGroup.Item>
-          <ListGroup.Item>
-            Birthday: {new Date(movie.director.birthday).toLocaleDateString()}
-          </ListGroup.Item>
-        </ListGroup>
-        <Button className='p-1 m-2 ' onClick={() => backButton()}>
-          Back
+export const MovieView = ({ movie, user, token, setUser }) => {
+  const { movieId } = useParams();
+  const navigate = useNavigate();
+  const [isInUserList, setIsInUserList] = useState(false);
+  const m = movie.find((m) => m.id === movieId);
+  console.log(m);
+
+  useEffect(() => {
+    if (m) {
+      const u = user.movieId.filter((userMovie) => userMovie.id !== m.id).includes(m.id);
+      setIsInUserList(!!u);
+      console.log(u);
+    }
+  }, [movie, user, movieId]);
+
+  const postMovie = () => {
+    const updateMovie = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${token}`,
+      },
+      body: JSON.stringify({
+        movieId: m.id,
+      }),
+    };
+    fetch(`https://appflixcf-d4726ef19667.herokuapp.com/users/${user.Username}`, updateMovie, {
+      Headers: { Authorization: `bearer ${token}` },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        alert('Movie added to your list');
+        setUser(data);
+        navigate(-1);
+      })
+      .catch((e) => {
+        console.log(e);
+        alert(`Error updating user info: ${e.message}`);
+      });
+  };
+
+  const delMovie = () => {
+    const removeMovie = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `bearer ${token}`,
+      },
+      body: JSON.stringify({
+        movieId: m.id,
+      }),
+    };
+    fetch(
+      `https://appflixcf-d4726ef19667.herokuapp.com/users/${encodeURIComponent(
+        user.Username
+      )}/movies/${encodeURIComponent(movieId)}`,
+      removeMovie
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        alert('Movie has been removed from your list');
+        console.log(data);
+        setUser(data);
+        navigate(-1);
+      })
+      .catch((e) => {
+        console.log(e);
+        alert(`Error in updating : ${e.message}`);
+      });
+  };
+
+  const addOrRemove = () => {
+    if (isInUserList) {
+      return (
+        <Button
+          className='float-right w-100 btn-secondary'
+          onClick={() => {
+            delMovie();
+            setIsInUserList(false);
+          }}>
+          Remove
         </Button>
-      </Card.Body>
-    </Card>
-  );
+      );
+    } else {
+      return (
+        <Button
+          className='float-right w-100 btn-secondary'
+          onClick={() => {
+            postMovie();
+            setIsInUserList(true);
+          }}>
+          Add
+        </Button>
+      );
+    }
+  };
+
+  {
+    return (
+      <Card className='movieViewCard  justify-self-center w-50 justify-content-center align-self-center p-1'>
+        {addOrRemove()}
+        <Card.Body
+          md={3}
+          sm={6}>
+          <Card.Img
+            md={3}
+            sm={6}
+            xs={12}
+            src={m.image}
+            style={{ width: '50%' }}
+            alt='Movie Title Image'
+          />
+          <Card.Title className='text-light'> Title: {movie.title}</Card.Title>
+          <ListGroup className='movieViewList'>
+            <ListGroup.Item> Genre: {m.genre}</ListGroup.Item>
+            <ListGroup.Item> Director: {m.director.name}</ListGroup.Item>
+            <ListGroup.Item>Bio: {m.director.bio}</ListGroup.Item>
+            <ListGroup.Item>
+              Birthday: {new Date(m.director.birthday).toLocaleDateString()}
+            </ListGroup.Item>
+          </ListGroup>
+          <Link to={''}>
+            <Button
+              onClick={(e) => {
+                e.preventDefault();
+                navigate(-1);
+              }}
+              className='p-1 m-2 '>
+              Back
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+    );
+  }
 };
 MovieView.propTypes = {
   movie: PropTypes.shape({
